77accf58cbb38abc6ccb4f8b1fc900af26a64000
Joan <joan.munoz.madero@gmail.com>
2022-06-06 19:12:23 +0200

    Starting the app class, session and router

--- index.php
+++ index.php
@@ -1,2 +1,10 @@
 <?php
-    
+/**
+* SoftPHP 
+* @author Joan Muñoz Madero
+* @version 0.0.0
+*/
+require_once __DIR__.'/app/SoftPHP/SoftPHP';
+use App\SoftPHP\SoftPHP as SoftPHP;
+$app= new SoftPHP();
+$app->executeURL();
